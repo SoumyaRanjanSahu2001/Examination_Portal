e0a9832375d382c9ec5059d2bc1f9c8e5ebc1f61
SoumyaRanjanSahu2001 <139950402+SoumyaRanjanSahu2001@users.noreply.github.com>
2023-07-26 15:16:17 +0530

    Update Email_Setup.js

--- Email_Setup.js
+++ Email_Setup.js
@@ -3,8 +3,7 @@
 // const transporter =  nodemailer.createTransport({
 //     service:"gmail",
 //     auth:{
-//         user:"soumyaranjansahuliku@gmail.com",
-//         pass:"fvhvnvgxrwyhtrxi"
+
 //     }
 // });
 
